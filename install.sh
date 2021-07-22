fde3246ad323adfc4366fa1103f2ac0697c69ea9
lunawDEV <87834094+lunawDEV@users.noreply.github.com>
2021-07-22 18:07:38 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #! /bin/sh
-git clone https://github.com/lunawDEV/BionicFetch.git
 sudo mv biofetch /bin/
 sudo chmod +x /bin/biofetch
 echo Sucessfully installed biofetch to your computer. To run biofetch, just type "biofetch" in your terminal.
