7272ad1ffb8d1a889606069af541e0afb0bc4ffc
lunawDEV <87834094+lunawDEV@users.noreply.github.com>
2021-07-22 17:56:36 -0400

    Added install.sh
    Added permissions to execute

---
+++ install.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+git clone https://github.com/lunawDEV/BionicFetch.git
+sudo mv biofetch /bin/
+sudo chmod +x /bin/biofetch
+echo Sucessfully installed biofetch to your computer. To run biofetch, just type "biofetch" in your terminal.
